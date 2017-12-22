96ab5ee68d1b28de2e60abe2b1af458d1766fb1d
keitheous <keithchongwy@gmail.com>
2017-12-22 21:24:04 +0800

    happy for now. moving onto task 3

--- ex5_text_based_adventure_game.rb
+++ ex5_text_based_adventure_game.rb
@@ -1,27 +1,27 @@
-# class Player
-#   attr_reader :location
-#
-#   def initialize(location)
-#     @location = location
-#   end
-#
-#   def look_around
-#     p "Location: #{self.location[:description]}"
-#     self.location[:items].each do |item|
-#       p "You see a #{item} on the floor"
-#     end
-#
-#     nil
-#   end
-#
-#   def pick_up(item)
-#     current_items = self.location[:items]
-#     current_items.delete_at(rand(current_items.length))
-#     current_items << item
-#
-#     "picked up #{item}"
-#   end
-# end
+class Player
+  attr_reader :location
+
+  def initialize(location)
+    @location = location
+  end
+
+  def look_around
+    p "Location: #{self.location[:description]}"
+    self.location[:items].each do |item|
+      p "You see a #{item} on the floor"
+    end
+
+    nil
+  end
+
+  def pick_up(item)
+    current_items = self.location[:items]
+    current_items.delete_at(rand(current_items.length))
+    current_items << item
+
+    "picked up #{item}"
+  end
+end
 
 # instructions for task 1
 
@@ -44,44 +44,46 @@ class Map
   end
 
   def describe
-    self.current_location[:description]
+    p self.current_location[:description]
   end
 
   def move_to(direction)
-    location_instance_with_edge =
+    location_instance_with_direction =
       locations.detect do |location|
         location[:edges].first[:direction] == direction
       end
 
     new_location_name =
-      location_instance_with_edge[:edges].first[:location]
+      location_instance_with_direction[:edges].first[:location]
 
     @current_location =
       locations.detect{|location| location[:name] == 'attic'}
   end
 end
 
-locations = [{
-    name: "living_room",
-    description: "You are in the living-room. A wizard is snoring loudly on the couch.",
-    items: ["whiskey", "bucket"],
-    edges: [{
-      direction: "upstairs",
-      item: "ladder",
-      location: "attic"
-    }]
-  },
-  {
-    name: "attic",
-    description: "You are in the attic. There is a giant welding torch in the corner.",
-    edges: [{
-      direction: "downstairs",
-      item: "ladder",
-      location: "living_room"
-    }]
-}]
+# locations = [{
+#     name: "living_room",
+#     description: "You are in the living-room. A wizard is snoring loudly on the couch.",
+#     items: ["whiskey", "bucket"],
+#     edges: [{
+#       direction: "upstairs",
+#       item: "ladder",
+#       location: "attic"
+#     }]
+#   },
+#   {
+#     name: "attic",
+#     description: "You are in the attic. There is a giant welding torch in the corner.",
+#     edges: [{
+#       direction: "downstairs",
+#       item: "ladder",
+#       location: "living_room"
+#     }]
+# }]
 
-map = Map.new(locations)
-map.describe
-map.move_to("upstairs")
+# instructions for task 2
+# map = Map.new(locations)
+# map.describe
+# p 'moving upstairs'
+# map.move_to("upstairs")
 # map.describe
